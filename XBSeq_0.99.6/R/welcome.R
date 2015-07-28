18a810b6c5c6a74221ed115bab684b2a420282ce
Yuanhang Liu <yuanhangliu.ok@gmail.com>
2015-07-28 17:20:58 -0500

    Updated to XBSeq_files which contains GTF file and scripts in generating these files

--- XBSeq_0.99.6/R/welcome.R
+++
@@ -1,9 +0,0 @@
-# ==========================================================================
-# package initialization
-# ==========================================================================
-.onAttach = function(libname, pkgname) {
-    msg = "Welcome to 'XBSeq'."
-    msg = strwrap(msg, exdent=4, indent=4)
-    packageStartupMessage(paste(msg, collapse="\n"), appendLF=TRUE)
-}
-
